3facc25b94897115fc9b9c5fa104f0369af1ae7c
Shashank Sharma <shashank.hacky0@gmail.com>
2024-02-19 20:43:04 +0530

    payment link updated

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ var btn = document.getElementById('btn')
 
 function leftClick() {
 	btn.style.left = '0'
-	window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315','_blank');
- window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315');
+	window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51','_blank');
+ window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51');
 }
 
 function rightClick() {
 	btn.style.left = '265px'
-     window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315','_blank');
- window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315');
+     window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51','_blank');
+ window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51');
 }
