7dfbb2f25044323f84142146d340e78cb04d2a5d
Shashank Sharma <shashank.hacky0@gmail.com>
2024-02-19 15:51:47 +0530

    fixed video controls

--- js/app.js
+++ js/app.js
@@ -7,6 +7,6 @@ function leftClick() {
 
 function rightClick() {
 	btn.style.left = '265px'
-//      window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51','_blank');
-//  window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51');
+     window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51','_blank');
+ window.open('https://cosmofeed.com/vig/65d3032f67e67d0013b79e51');
 }
