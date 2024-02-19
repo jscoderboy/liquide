ba983f7f4714e848418e8c0a635c433b0afe60a0
Shashank Sharma <shashank.hacky0@gmail.com>
2024-02-19 19:09:37 +0530

    added link to both button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var btn = document.getElementById('btn')
 
 function leftClick() {
 	btn.style.left = '0'
+	window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315','_blank');
+ window.open('https://cosmofeed.com/vig/65c5be57f26672001eedf315');
 }
 
 function rightClick() {
